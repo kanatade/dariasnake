480d7f82fbf3276e5af63ec5a558eb88a85066b8
User <zubova.daria@yahoo.com>
2019-03-01 18:05:46 -0800

    final data

--- app/dummy.py
+++ app/dummy.py
@@ -54,92 +54,105 @@ def start():
 
     return start_response(color, headType, tailType)
 
+def init(data):
+	print("init")
+	print("=================\n")
+	datastring = json.dumps(data)
+	datastore = json.loads(datastring)
+	# print(datastore)
+
+	myhead = list(datastore['you']['body'][0].values())
+	mybody = []
+
+	for coords in datastore['you']['body']:
+		mybody.append(list(coords.values()))
+	print("myhead\n" + "===========\n" + str(myhead) + "\n")
+	print("mybody\n" + "===========\n" + str(mybody) + "\n")
+
+	distinctsnakexy = []
+	snakexy = []
+	snakehead = []
+	# snakexy = datastore["board"]["snakes"]['body']
+
+	for snake in datastore['board']['snakes']:
+		onesnakexy = [] # one snake's body
+		for coords in snake['body']:
+			onesnakexy.append(list(coords.values()))  # append each coords of snake's body to that particular snake
+			snakexy.append(list(coords.values()))  # append each coords of snake's body to that particular snake
+		distinctsnakexy.append(onesnakexy)
+		# append all snakes body to an array of snake bodies (eachcoords array in onesnakebody array in allsnake
+		# array) (3dArray)
+		snakehead.append(list(snake['body'][0].values()))
+		# append all snakes head coordinates to an array of snake heads (eachcoordsofhead array in allsnakearray) (2dArray)
+
+	print("snakexy\n" + "===========\n" + str(snakexy) + "\n")
+
+	# snakexy[0][0] is x, snakexy[0][1] is y ; distinctsnakexy[0] is myself, snakexy[0][0] is my head, snakexy[0][0][0]
+	# is my head's x, snakexy[0][0][1] is my head's y
+
+	height = datastore["board"]["height"]
+	width = datastore["board"]["width"]
+
+	wall = []  # 2d array of coordinates
+
+	for i in range(0,height):
+		wall.append([-1,i])
+
+	for i in range(0,height):
+		wall.append([width-1, i])
+
+	for i in range(1, width - 1):
+		wall.append([i,0])
+
+	for i in range(1, width - 1):
+		wall.append([i,height-1])
+
+	print("walls\n" + "==========\n" + str(wall) + "\n")
+
+	return wall, myhead, mybody, snakehead, snakexy, height, width
+
 
 @bottle.post('/move')
 def move():
-    data = bottle.request.json
-    """
-    TODO: Using the data from the endpoint request object, your
-        snake AI must choose a direction to move in.
-        """
-    print("move part")
-    print("================")
-    # print(json.dumps(data))
-    json_string = json.dumps(data)
-    game_data = json.loads(json_string)
-    height = game_data["board"]["height"]
-    width = game_data["board"]["width"]
+	data = bottle.request.json
+	"""
+	TODO: Using the data from the endpoint request object, your
+			snake AI must choose a direction to move in.
+	"""
+	print("move part")
+	print("================\n")
+	wall, myhead, mybody, snakehead, snakexy, height, width = init(data)
 
-    # to get my position
-    my_position_x = []
-    my_position_y = []
-    for i in range(0, len(game_data["you"]["body"])):
-        my_position_x.append(int(game_data["you"]["body"][i]["x"]))
-        my_position_y.append(int(game_data["you"]["body"][i]["y"]))
+	safe = []
 
-    print(my_position_x[0])
-    print(my_position_y[0])
-    print(height)
-    print(width)
+	# avoid all obstacles
+	right = [myhead[0] + 1, myhead[1]]
+	left = [myhead[0] - 1, myhead[1]]
+	down = [myhead[0], myhead[1] + 1]
+	up = [myhead[0], myhead[1] - 1]
 
-    #targeting other snakes' heads
+	if right not in snakexy and right[0] != height:  # right direction
+		# right is safe
+		safe.append("right")
+	if left not in snakexy and left[0] != -1:
+		safe.append("left")
+	if down not in snakexy and down[1] != height:
+		safe.append("down")
+	if up not in snakexy and up[1] != -1:
+		safe.append("up")
 
+	# 1. Check every point starting from one corner and moving to the other, in either rows or columns, it doesn't
+	# matter. Once you reach a point that has three or more orthogonally adjacent walls, mark that point as a dead
+	# end, and go to 2.
+	# 2. Find the direction of the empty space next to this point (if any), and check every point in
+	#  that direction. For each of those points: if it has two or more adjacent walls, mark it as a dead end. If it
+	# has only one wall, go to 3. If it has no walls, stop checking in this direction and continue with number 1.
+	# 3. In every direction that does not have a wall, repeat number 2.
 
-    # try to not hit wall
-    # direction = random.choice(directions)
-    directions = ['up', 'down', 'left', 'right']
-    if my_position_x[0] == 0:
-        if my_position_y[0] == 0:
-            if my_position_x[1] == 0:
-                direction = 'down'
-            else:
-                direction = 'right'
-        elif my_position_y[0] == height - 1:
-            if my_position_x[1] == 1:
-                direction = 'up'
-            else:
-                direction = 'right'
-        elif my_position_x[1] == 1:
-            direction = 'up'
-        else:
-            direction = 'right'
-    elif my_position_x[0] == width - 1:
-        if my_position_y[0] == 0:
-            if my_position_x[1] == width - 2:
-                direction = 'down'
-            else:
-                direction = 'left'
-        elif my_position_y[0] == height - 1:
-            if my_position_y[1] == height - 2:
-                direction = 'left'
-            else:
-                direction = 'up'
-        elif my_position_x[1] == width - 2:
-            direction = 'up'
-        else:
-            direction = 'left'
-    elif my_position_y[0] == 0 and my_position_x[0] != 0 and my_position_x[0] != width - 1:
-        if my_position_y[1] == 1:
-            direction = 'left'
-        else:
-            direction = 'down'
-    elif my_position_y[0] == height - 1 and my_position_x[0] != 0 and my_position_x[0] != width - 1:
-        if my_position_y[1] == height - 2:
-            direction = 'right'
-        else:
-            direction = 'up'
-    else:
-        if my_position_x[1] == my_position_x[0] - 1:
-            direction = 'right'
-        elif my_position_x[1] == my_position_x[0] + 1:
-            direction = 'left'
-        elif my_position_y[1] == my_position_y[0] - 1:
-            direction = 'down'
-        else:
-            direction = 'up'
+	direction = random.choice(safe)
 
-    print(direction)
-    return move_response(direction)
+	print("moveresponse\n" + "==========\n" + str(direction) + "\n")
+	return move_response(direction)
 
 
 @bottle.post('/end')
